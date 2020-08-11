d57a545da49583107661edd25b2f14fdf602d02c
SOUMITRA1208 <69211643+SOUMITRA1208@users.noreply.github.com>
2020-08-11 06:19:41 -0700

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,15 @@
+import datetime
+import winsound
+
+message=input('Enter your alarm message ')
+alarmhour=int(input('At what hour do you want to wake up '))
+alarmminute=int(input('At what minute do you want to wake up '))
+ampm=str(input("am or pm ? "))
+if ampm=='pm':
+	alarmhour=alarmhour+12
+
+while(1==1): 
+    if (alarmhour == datetime.datetime.now().hour and alarmminute == datetime.datetime.now().minute):
+    	print(message)
+    	
+    	winsound.PlaySound("iichy",winsound.SND_FILENAME)
